5cfd4686415c38453fc082f6009db6198a70d2f4
Алексей Никандров <alexey.nikandrov@sredasolutions.com>
2024-12-03 12:29:16 +0300

    lecture_02_hw_02

--- lecture_02_data_structures_functions/hw/tasks/hw1.py
+++ lecture_02_data_structures_functions/hw/tasks/hw1.py
@@ -7,6 +7,7 @@ Given a file containing text. Complete using only default collections:
     5) Find most common non ascii char for document
 """
 from typing import List
+from typing import Dict
 
 
 def get_longest_diverse_words(file_path: str) -> List[str]:
@@ -14,12 +15,35 @@ def get_longest_diverse_words(file_path: str) -> List[str]:
 
 
 def get_rarest_char(file_path: str) -> str:
-    ...
+    res = {}
+    with open(file_path, "r") as f:
+        for i in f.readlines():
+            for j in i.split():
+                for k in j:
+                    if k not in res:
+                        res[k] = 1
+                    else:
+                        res[k] += 1
+    ans = [1e15, ""]
+    for i in res:
+        if res[i] < ans[0]:
+            ans[0] = res[i]
+            ans[1] = i
+
+    return ans[1]
 
 
-def count_punctuation_chars(file_path: str) -> int:
-    ...
+def count_punctuation_chars(file_path: str) -> Dict[str, int]:
+    punctuation = {"!", "'", '"', ",", ".", "?", ":", ";", "-"}
+    ans = dict.fromkeys(punctuation, 0)
+    with open(file_path, "r") as f:
+        for i in f.readlines():
+            for j in i.split():
+                for k in j:
+                    if k in punctuation:
+                        ans[k] += 1
 
+    return ans
 
 def count_non_ascii_chars(file_path: str) -> int:
     ...
