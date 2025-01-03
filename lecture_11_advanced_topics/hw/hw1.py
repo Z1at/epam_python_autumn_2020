ac61983ff2c0bbb174c615ad6bd04aa86c661fb2
1at <nikandrov2103@gmail.com>
2025-01-04 00:02:10 +0300

    lecture_11_hw_1

--- lecture_11_advanced_topics/hw/hw1.py
+++ lecture_11_advanced_topics/hw/hw1.py
@@ -30,9 +30,45 @@ class SizesEnum(metaclass=SimplifiedEnum):
     __keys = ("XL", "L", "M", "S", "XS")
 
 
-assert ColorsEnum.RED == "RED"
-assert SizesEnum.XL == "XL"
+>>> assert ColorsEnum.RED == "RED"
+>>> assert SizesEnum.XL == "XL"
+
 """
 
 
+class SimplifiedEnum(type):
+    """Simulates the work enum.py.
+    Attention: members must be passed through the __keys class attribute.
 
+    Args:
+        *args: Special child class data. Not transmitted directly.
+        **kwargs: Special child class data. Not transmitted directly.
+
+    Attributes:
+        child_name_plus_child_keys_var: Contains the class name and class attribute.
+
+    """
+
+    def __new__(mcs, name, bases, class_dict):
+
+        cls_instance = super().__new__(mcs, name, bases, class_dict)
+
+        cls_instance.__keys = class_dict[f"_{name}__keys"]
+        for key in cls_instance.__keys:
+            setattr(cls_instance, key, key)
+
+        return cls_instance
+
+    def __iter__(cls):
+        yield from cls.__keys
+
+    def __len__(cls):
+        return len(cls.__keys)
+
+
+class ColorsEnum(metaclass=SimplifiedEnum):
+    __keys = ("RED", "BLUE", "ORANGE", "BLACK")
+
+
+class SizesEnum(metaclass=SimplifiedEnum):
+    __keys = ("XL", "L", "M", "S", "XS")
--- lecture_11_advanced_topics/hw/hw2.py
+++ lecture_11_advanced_topics/hw/hw2.py
@@ -23,8 +23,80 @@ def elder_discount(order):
     ...
 
 order_1 = Order(100, morning_discount)
-assert order_1.final_price() == 75
+assert order_1.final_price() == 50
 
 order_2 = Order(100, elder_discount)
-assert order_2.final_price() == 10
+assert order_1.final_price() == 10
+
 """
+from __future__ import annotations
+
+
+class Discount:
+    """The strategy interface declares operations common to all
+    supported versions of some algorithm. The context uses this
+    interface to call the algorithm defined by the concrete strategies.
+
+    """
+
+    @classmethod
+    def __init_subclass_(cls):
+        if not hasattr(cls, "discount"):
+            raise NotImplementedError(
+                f"Class {cls} lacks required discount class attribute"
+            )
+
+    @staticmethod
+    def final_price(order: Order) -> int:
+        return order.price - order.price * order.discount_program.discount
+
+
+class NoDiscount(Discount):
+
+    discount = 0
+
+
+class MorningDiscount(Discount):
+
+    discount = 0.5
+
+
+class ElderDiscount(Discount):
+
+    discount = 0.9
+
+
+class Order:
+    """Gets the order price and discount program.
+    When calling the final_price method, the order price is returned.
+
+    Args:
+        price: The order price.
+        discount_program: The desired discount program.
+
+    Attributes:
+        price: The order price.
+        discount_program: The desired discount program.
+
+    """
+
+    def __init__(self, price, discount_program):
+        self.price = price
+        self.discount_program = discount_program
+
+    def final_price(self):
+        return self.discount_program.final_price(self)
+
+
+if __name__ == "__main__":
+    order1 = Order(100, NoDiscount)
+    # print(order1.final_price())
+    # print(order1.final_price())
+
+    order1.discount_program = MorningDiscount
+
+    # order2 = Order(100, "morning_discount")
+    # print(order1.final_price())
+
+    order3 = Order(100, ElderDiscount)
+    # print(order3.final_price())
